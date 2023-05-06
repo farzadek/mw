ba9ae686c967991b6caf6978a3b916e25847cebf
“farzadek” <farzadek@gmail.com>
2023-05-06 09:29:45 -0400

    contact

--- api/sendMail.php
+++ api/sendMail.php
@@ -1,5 +1,6 @@
 <?php
 header('Access-Control-Allow-Origin: *');
+
 $who = isset($_GET['who']) && !empty($_GET['who']) ? filter_var(htmlentities(trim($_GET['who'])), FILTER_FLAG_STRIP_HIGH) : '';
 $name = isset($_GET['name']) && !empty($_GET['name']) ? filter_var(htmlentities(trim($_GET['name'])), FILTER_FLAG_STRIP_HIGH) : '';
 $phone = isset($_GET['phone']) && !empty($_GET['phone']) ? filter_var(htmlentities(trim($_GET['phone'])), FILTER_FLAG_STRIP_HIGH) : '';
@@ -15,17 +16,13 @@ if ($message == '') {
     $subject = 'Message received from montrealweb-contact';
     $reply = '<p>Name: ' . $name . '</p><p>Phone: ' . $phone . '</p><p>Email: ' . $email . '</p><p>Message: ' . $message . '</p><p>Lang: ' . $lang . '</p>';
     $headers .= 'From: montrealweb - contact' . "\r\n";
-    if (mail('farzadek@gmail.com', $subject, $reply, $headers)) {
-        $result = true;
-    }
+    $result = mail('farzadek@gmail.com', $subject, $reply, $headers);
 } else {
     if (!empty($email)) {
         $subject = $lang == 'en' ? 'Your message received' : 'On a recu votre message';
         $reply = $lang == 'en' ? '<p>We received your message, we will contact you as soon as possible.</p>MontrealWeb.ca<br/>(438) 300-0456' : '<p>Nous avons recu votre message, nous serons contactés dès que possible.</p>MontrealWeb.ca<br/>(438) 300-0456';
         $headers .= 'From: MontrealWeb.ca' . "\r\n";
-        if (mail($email, $subject, $reply, $headers)) {
-            $result = true;
-        }
+        $result = mail($email, $subject, $reply, $headers);
     }
 }
 
